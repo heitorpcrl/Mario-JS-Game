bfe46b57270a1c60b18bd772ebbf14971252bc82
Heitor Cortes <heitorphantom@gmail.com>
2025-05-12 11:49:34 -0300

    added keydown features in script file

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-confirm
+const mario = document.querySelector('.mario');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 900);
+}
+
+document.addEventListener('keydown', jump);
