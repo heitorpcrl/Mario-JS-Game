04f6f709ea88daa27d71e95214b6744906a6ec6d
Heitor Cortes <heitorphantom@gmail.com>
2025-05-19 20:54:50 -0300

    feat: Added score board and fixed jump cooldown

--- JS/animation-controller.js
+++ JS/animation-controller.js
@@ -1,15 +1,29 @@
 const mario = document.querySelector('.mario');
+let isJumping = false;
+let score = 0;
+let scoreElement = document.querySelector('.score-board p');
+
+const updateScore = () => {
+    score++;
+    scoreElement.textContent = `Pontuação: ${String(score).padStart(2, '0')}`;
+}
+
+setInterval(updateScore, 1500);
 
 const jump = () => {
+    if (isJumping) return;
+    
+    isJumping = true;
     mario.classList.add('jump');
 
     setTimeout(() => {
-
         mario.classList.remove('jump');
-
+        isJumping = false;
     }, 900);
 }
 
-document.addEventListener('keydown', jump)
+document.addEventListener('keydown', jump);
+
+
 
 
